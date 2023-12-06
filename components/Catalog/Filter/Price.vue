6942326205912c1264b36026d6a9274c0d560d4a
Sergey <ser12563208@yandex.ru>
2023-12-06 17:49:26 +0400

    + filter price classes + value check

--- components/Catalog/Filter/Price.vue
+++ components/Catalog/Filter/Price.vue
@@ -10,6 +10,9 @@ const props = defineProps({
     type: Array,
     default: () => [null, null],
   },
+  headerClass: String,
+  bodyClass: String,
+  bottomClass: String,
 });
 const emit = defineEmits(["change", "update:modelValue"]);
 
@@ -18,10 +21,16 @@ const rangeValue = ref([
   props.modelValue[1] || props.max,
 ]);
 
-function update(val: number[]) {
-  rangeValue.value = val;
-  emit("update:modelValue", val);
-  emit("change", val);
+function update(val, index: number) {
+  if (!!index?.toString()) {
+    let tmp_arr = [...rangeValue.value];
+    tmp_arr[index] = val;
+    rangeValue.value = tmp_arr;
+  } else {
+    rangeValue.value = val;
+  }
+  emit("update:modelValue", rangeValue.value);
+  emit("change", rangeValue.value);
 }
 function filterChange(e) {
   const filerParam = {
@@ -30,35 +39,50 @@ function filterChange(e) {
   };
   emit("update:modelValue", filerParam);
 }
-
-watch(
-  () => rangeValue,
-  (val) => {
-    console.log(val, "price changed");
-    filterChange(val);
-  }
-);
 </script>
 
 <template>
   <div class="filter-price">
-    <slot name="header"><p>Цена</p></slot>
-    <div class="d-flex">
-      <slot name="input-min-price" v-bind="filterChange">
+    <div class="filter-price__header" :class="headerClass">
+      <slot name="header">
+        <p class="filter-price__title">Цена</p>
+      </slot>
+    </div>
+    <div class="d-flex filter-price__form-body" :class="bodyClass">
+      <slot
+        name="input-min-price"
+        v-bind="{ filterChange, rangeValue, min, max, update }"
+      >
         <InputField v-model.lazy.number="rangeValue[0]" />
       </slot>
-      <slot name="input-max-price" v-bind="filterChange">
+      <slot
+        name="input-max-price"
+        v-bind="{ filterChange, rangeValue, min, max, update }"
+      >
         <InputField v-model.lazy.number="rangeValue[1]" />
       </slot>
       <slot name="currency">
         <span>{{ currency }}</span>
       </slot>
     </div>
-    <slot
-      name="after"
-      v-bind="{ filterChange, min, max, rangeValue, update }"
-    />
+    <div class="filter-price__bottom" :class="bottomClass">
+      <slot
+        name="after"
+        v-bind="{ filterChange, min, max, rangeValue, update }"
+      />
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.filter-price {
+  &__header {
+  }
+  &__title {
+  }
+  &__form-body {
+  }
+  &__bottom {
+  }
+}
+</style>
