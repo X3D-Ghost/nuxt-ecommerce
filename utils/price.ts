95ea38cedd906b55434ec90e3a04277b15654a36
Sergey <ser12563208@yandex.ru>
2024-02-04 01:17:41 +0400

    add: price format

--- utils/price.ts
+++ utils/price.ts
@@ -1,7 +1,10 @@
 export const computeDiscount = (newPrice: number, oldPrice: number): number =>
-  100 - (newPrice * 100) / oldPrice;
+  100 - Math.round((newPrice * 100) / oldPrice);
 
 export const computeDiscountWPercent = (
   newPrice: number,
   oldPrice: number
 ): string => `-${computeDiscount(newPrice, oldPrice)}%`;
+
+export const formatPrice = (price: string): string =>
+  price.replace(/\B(?=(\d{3})+(?!\d))/g, " ");
